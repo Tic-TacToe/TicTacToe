59d6b1c3014498f538f281e1da7fd3826c88b20a
sylv0r <sylvian.vidal95@gmail.com>
2023-01-05 16:20:40 +0100

    ascii

--- morpion/src/main/java/com/codingf/morpion/game/Game.java
+++ morpion/src/main/java/com/codingf/morpion/game/Game.java
@@ -19,8 +19,8 @@ public class Game {
 
     public Game() {
         this.grid = new Grid();
-        this.player1 = new Player(0 ,'O');
-        this.player2 = new Player(1 ,'X');
+        this.player1 = new Player(0 ,'X');
+        this.player2 = new Player(1 ,'O');
 
     }
 
@@ -40,17 +40,23 @@ public class Game {
         int input = -1;
         while (true){
             System.out.println("Joueur " + this.currentPlayer.getToken() + " a ton tour");
+
+
             input = Input.readInt(scan);
+
+
 
             if (0 <= input && input<= 8){
                 break;
             }
+
         }
         return input;
 
     }
 
     //methode
+
     private void printGrid(){
         System.out.flush();
         System.out.println(this.grid);
@@ -64,47 +70,112 @@ public class Game {
 
             int input = getUserInput(scan);
 
-            this.grid.setCaseToken(input, this.currentPlayer.getToken());
+            if (!grid.isCaseUsed(input)) {
+                // Marque la case avec le jeton du joueur
+                this.grid.setCaseToken(input, this.currentPlayer.getToken());
+            } else {
+                // La case a déjà été jouée, on demande au joueur de choisir une autre case
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+                System.out.println("La case est deja prise !");
+
+                continue;
+            }
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+
+
+
+
+
 
             if (this.grid.getWinner() != null){
                 //joueur qui gagne
                 printGrid();
                 if (this.currentPlayer.getToken() == this.player1.getToken()){
-                    System.out.println(" ▄▄▄▄▄▄▄▄▄▄▄                                ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄ \n" +
-                            "▐░░░░░░░░░░░▌                              ▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░▌      ▐░▌\n" +
-                            "▐░█▀▀▀▀▀▀▀█░▌                              ▐░▌       ▐░▌ ▀▀▀▀█░█▀▀▀▀ ▐░▌░▌     ▐░▌\n" +
-                            "▐░▌       ▐░▌                              ▐░▌       ▐░▌     ▐░▌     ▐░▌▐░▌    ▐░▌\n" +
-                            "▐░▌       ▐░▌                              ▐░▌   ▄   ▐░▌     ▐░▌     ▐░▌ ▐░▌   ▐░▌\n" +
-                            "▐░▌       ▐░▌                              ▐░▌  ▐░▌  ▐░▌     ▐░▌     ▐░▌  ▐░▌  ▐░▌\n" +
-                            "▐░▌       ▐░▌                              ▐░▌ ▐░▌░▌ ▐░▌     ▐░▌     ▐░▌   ▐░▌ ▐░▌\n" +
-                            "▐░▌       ▐░▌                              ▐░▌▐░▌ ▐░▌▐░▌     ▐░▌     ▐░▌    ▐░▌▐░▌\n" +
-                            "▐░█▄▄▄▄▄▄▄█░▌                              ▐░▌░▌   ▐░▐░▌ ▄▄▄▄█░█▄▄▄▄ ▐░▌     ▐░▐░▌\n" +
-                            "▐░░░░░░░░░░░▌                              ▐░░▌     ▐░░▌▐░░░░░░░░░░░▌▐░▌      ▐░░▌\n" +
-                            " ▀▀▀▀▀▀▀▀▀▀▀                                ▀▀       ▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀ \n" +
-                            "                                                                                  ");
+                    System.out.print("                                                                 \n" +
+                            "                                                                 \n" +
+                            "                                                                 \n" +
+                            "                                                                 \n" +
+                            "                                                                 \n");
+                    System.out.println("=============================================================================================================================================================");
+                    System.out.println("                             _          _                                __   __             _         \n" +
+                            "                            | |        (_)                               \\ \\ / /            (_)        \n" +
+                            "                            | | ___     _  ___  _   _  ___ _   _ _ __     \\ V /    __      ___ _ __    \n" +
+                            "                            | |/ _ \\   | |/ _ \\| | | |/ _ \\ | | | '__|     > <     \\ \\ /\\ / / | '_ \\   \n" +
+                            "                            | |  __/   | | (_) | |_| |  __/ |_| | |       / . \\     \\ V  V /| | | | |  \n" +
+                            "                            |_|\\___|   | |\\___/ \\__,_|\\___|\\__,_|_|      /_/ \\_\\     \\_/\\_/ |_|_| |_|  \n" +
+                            "                                      _/ |                                                             \n" +
+                            "                                     |__/                                                              ");
+                    System.out.println("=============================================================================================================================================================");
+
                 }else {
-                    System.out.println(" ▄       ▄                                ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄ \n" +
-                            "▐░▌     ▐░▌                              ▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░▌      ▐░▌\n" +
-                            " ▐░▌   ▐░▌                               ▐░▌       ▐░▌ ▀▀▀▀█░█▀▀▀▀ ▐░▌░▌     ▐░▌\n" +
-                            "  ▐░▌ ▐░▌                                ▐░▌       ▐░▌     ▐░▌     ▐░▌▐░▌    ▐░▌\n" +
-                            "   ▐░▐░▌                                 ▐░▌   ▄   ▐░▌     ▐░▌     ▐░▌ ▐░▌   ▐░▌\n" +
-                            "    ▐░▌                                  ▐░▌  ▐░▌  ▐░▌     ▐░▌     ▐░▌  ▐░▌  ▐░▌\n" +
-                            "   ▐░▌░▌                                 ▐░▌ ▐░▌░▌ ▐░▌     ▐░▌     ▐░▌   ▐░▌ ▐░▌\n" +
-                            "  ▐░▌ ▐░▌                                ▐░▌▐░▌ ▐░▌▐░▌     ▐░▌     ▐░▌    ▐░▌▐░▌\n" +
-                            " ▐░▌   ▐░▌                               ▐░▌░▌   ▐░▐░▌ ▄▄▄▄█░█▄▄▄▄ ▐░▌     ▐░▐░▌\n" +
-                            "▐░▌     ▐░▌                              ▐░░▌     ▐░░▌▐░░░░░░░░░░░▌▐░▌      ▐░░▌\n" +
-                            " ▀       ▀                                ▀▀       ▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀ \n" +
-                            "                                                                                ");
+                    System.out.println("=============================================================================================================================================================");
+
+                    System.out.println("                             _          _                                  ____               _         \n" +
+                            "                            | |        (_)                                / __ \\             (_)        \n" +
+                            "                            | | ___     _  ___  _   _  ___ _   _ _ __    | |  | |   __      ___ _ __    \n" +
+                            "                            | |/ _ \\   | |/ _ \\| | | |/ _ \\ | | | '__|   | |  | |   \\ \\ /\\ / / | '_ \\   \n" +
+                            "                            | |  __/   | | (_) | |_| |  __/ |_| | |      | |__| |    \\ V  V /| | | | |  \n" +
+                            "                            |_|\\___|   | |\\___/ \\__,_|\\___|\\__,_|_|       \\____/      \\_/\\_/ |_|_| |_|  \n" +
+                            "                                      _/ |                                                              \n" +
+                            "                                     |__/                                                               ");
                 }
+                System.out.println("=============================================================================================================================================================");
+
 
                 break;
             }else if (this.grid.isFull()){
                 // match nul
                 printGrid();
-                System.out.println("  Match nul  :/");
+                System.out.println("=============================================================================================================================================================");
+
+                System.out.println("                                        _       _                     _   \n" +
+                        "                                       | |     | |                   | |  \n" +
+                        "                        _ __ ___   __ _| |_ ___| |__      _ __  _   _| |  \n" +
+                        "                       | '_ ` _ \\ / _` | __/ __| '_ \\    | '_ \\| | | | |  \n" +
+                        "                       | | | | | | (_| | || (__| | | |   | | | | |_| | |  \n" +
+                        "                       |_| |_| |_|\\__,_|\\__\\___|_| |_|   |_| |_|\\__,_|_|  \n" +
+                        "                                                                          \n" +
+                        "                                                                          ");
+                System.out.println("=============================================================================================================================================================");
+
 
                 break;
             }
+            Scanner scanner = new Scanner(System.in);
+
+            while (true) {
+                // Joue une partie
+
+                // Demande au joueur s'il souhaite rejouer
+                System.out.print("Voulez-vous rejouer ? (o/n) ");
+                String answer = scanner.nextLine();
+
+                if (answer.equals("n")) {
+                    // Le joueur ne souhaite pas rejouer, on sort de la boucle
+                    break;
+                } else if (!answer.equals("o")) {
+                    // La réponse du joueur n'est pas valide, on recommence
+                    Game game = new Game();
+                    System.out.println("===========================================================================================================================================================");
+                    System.out.println("║                                     _______         ______              ______                                                                          ║\n" +
+                            "║                                    /_  __(_)____   /_  __/___ ______   /_  __/___  ___                                                                  ║\n" +
+                            "║                                     / / / / ___/    / / / __ `/ ___/    / / / __ \\/ _ \\                                                                 ║\n" +
+                            "║                                    / / / / /__     / / / /_/ / /__     / / / /_/ /  __/                                                                 ║\n" +
+                            "║                                   /_/ /_/\\___/    /_/  \\__,_/\\___/    /_/  \\____/\\___/                                                                  ║\n" +
+                            "║                                                                                                                                                         ║");
+                    System.out.println("===========================================================================================================================================================");
+
+                    System.out.println("Bonjour et bienvenue dans le fameux jeu Tic Tac Toe (ou Morpion pour les intimes).");
+                    System.out.println("Le but est simple : remplire une ligne ou une colonne ou une diagonale avec son symbole ( X ou O)");
+                    System.out.println("c'est le joureur X qui commence. Bonne chance!");
+                    game.start();
+                }
+            }
+
+            scanner.close();
+
             swapTurn();
         }
         scan.close();
--- morpion/src/main/java/com/codingf/morpion/grid/Grid.java
+++ morpion/src/main/java/com/codingf/morpion/grid/Grid.java
@@ -1,6 +1,8 @@
 package com.codingf.morpion.grid;
 
 import com.codingf.morpion.player.Player;
+import com.codingf.morpion.game.Game;
+
 
 public class Grid {
     //champ
@@ -15,7 +17,7 @@ public class Grid {
 
         for (int i = 0; i <this.cases.length ; i++) {
             if (this.cases[i] == null) {
-                this.cases[i] = new GridCase(i);
+                this.cases[i] = new GridCase(i+1);
             }
 
         }
@@ -25,9 +27,9 @@ public class Grid {
 
     private Player getTokenOwnerOf(int id){
         return this.cases[id].getTokenOwner();
-
-
     }
+
+
 
     public void setCaseToken(int caseId, Token token){
         this.cases[caseId].setToken(token);
@@ -77,6 +79,11 @@ public class Grid {
     }
 
     //methode
+
+    public boolean isCaseUsed(int caseId) {
+        return !this.cases[caseId].isEmpty();
+    }
+
 
     public Player getWinner(){
 
